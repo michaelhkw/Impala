856d37baa23a1afbc9a164b514c5a8f02440b061
Michael Ho <kwho@cloudera.com>
2017-05-13 23:51:31 -0700

    IMPALA-4192: Disentangle Expr and ExprContext
    This change separates Expr and ExprContext. This is a preparatory
    step for factoring out static data (e.g. Exprs) of plan fragments
    to be shared by multiple plan fragment instances.
    
    This change includes the followings:
    
    1. Include aggregate functions (AggFn) as Expr. This separates
       AggFn from its evaluator. AggFn is similar to existing Expr
       as both are represented as a tree of Expr nodes but it doesn't
       really make sense to call Get*Val() on AggFn. This change
       restructures the class hierarchy: much of the existing Expr
       class is now renamed to ScalarExpr. Expr is the parent class
       of both AggFn and ScalarExpr. Expr is defined to be a tree
       with root of either AggFn or ScalarExpr and all descendants
       being ScalarExpr.
    
    2. ExprContext is renamed to ScalarExprEvaluator which is the
       interface for evaluating ScalarExpr; AggFnEvaluator is the
       interface for evaluating AggFn. Multiple evaluators can be
       instantiated per Expr. Expr contains static states of an
       expression while evaluator contains runtime states needed
       for execution (i.e. evaluating the expression).
    
    3. Update all exec nodes to instantiate Expr and their evaluators
       separately. ExecNode::Init() will be responsible for creating
       all the Exprs in an ExecNode while their evaluators are created
       in ExecNode::Prepare(). Certain evaluators are also moved into
       the data structures which actually utilize them. For instance,
       HashTableCtx now owns the build and probe expression evaluators.
       Similarly, TupleRowComparator and Sorter also own the evaluators.
       ExecNode which utilizes these data structures are only responsible
       for creating the expressions used by these data structures.
    
    4. All codegen functions take Exprs instead of evaluators. Also, codegen
       functions will not return error status should the IR function fails the
       LLVM verification step.
    
    5. The assignment of index into the FunctionContext vector is now done
       during the construction of ScalarExpr. Evaluators are only responsible
       for allocating and initializing the FunctionContexts.
    
    6. Open(), Prepare() are now removed from Expr classes. The interface
       for creating any Expr is via either ScalarExpr::Create() or AggFn::Create()
       which will convert a thrift Expr into an initialized Expr object.
       Similarly, Create() interface is used for creating evaluators from an
       intialized Expr object.
    
    This separation allows the future change to introduce PlanNode data structures.
    The plan is to move all ExecNode::Init() logic to PlanNode and call them once
    per plan fragment.
    
    Change-Id: Iefdc9aeeba033355cb9497e3a5d2363627dcf2f3

--- common/thrift/generate_error_codes.py
+++ common/thrift/generate_error_codes.py
@@ -319,6 +319,9 @@ error_codes = (
   ("KUDU_TIMESTAMP_OUT_OF_RANGE", 103,
    "Kudu table '$0' column '$1' contains an out of range timestamp. "
    "The valid date range is 1400-01-01..9999-12-31."),
+
+  ("IR_VERIFY_FAILED", 104,
+   "Failed to verify generated IR function $0, see log for more details."),
 )
 
 import sys
