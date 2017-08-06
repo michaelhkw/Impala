1955ed727307c69aff14116067412c6a9441afaa
Michael Ho <kwho@cloudera.com>
2017-08-05 17:23:31 -0700

    KUDU-2065, KUDU-2011: Release sidecars on cancellation or timeout
    Previously, when an outbound call times out after transmission
    of the request has begun but not finished, the RPC layer will
    continue to send the entire payload (including any sidecars) to
    the remote destination until the promised number of bytes specified
    in the header are sent. This is problematic for the users of the
    RPC layer as there is no well defined point in which the sidecars
    are no longer referenced by the RPC layer. The only model in which
    this would work is for the caller to either transfer or share ownership
    of the sidecars with the RPC layer. This has caused some life cycle
    issues with row batches in Impala before (e.g. IMPALA-5093).
    
    This change fixes the problem above by modifying the RPC protocol
    to allow a mid-transmission RPC call to be cancelled. Specifically,
    a new footer is added to all outbound call requests. It contains
    a flag, when true, indicates the outbound call was cancelled after
    it has started but before it finished. The server will inspect this
    flag and ignore inbound calls with this flag set. This footer enables
    the caller to relinquish references to the sidecars early when an
    outbound call is cancelled or timed-out. Once the call is cancelled
    or timed-out, the RPC layer will send the remainder of the bytes for
    the request with some dummy bytes. Since the server always ignores
    cancelled call requests, it's okay to send random bytes.
    
    To avoid breaking compatibility, a new RPC feature flag REQUEST_FOOTERS
    is introduced in this change. During connection negotiation, the
    client will always include this flag in its feature set. A server which
    supports parsing footer will include REQUEST_FOOTERS in its feature set
    if it sees the client also supports it. A client will only send the
    footer if the remote server has the RPC feature flag REQUEST_FOOTERS.
    
    Change-Id: I5c8e294090279649082eebc4f6cfb6fe858bbbfc
    Reviewed-on: http://gerrit.cloudera.org:8080/7599
    Reviewed-by: Todd Lipcon <todd@apache.org>
    Tested-by: Kudu Jenkins

--- be/src/kudu/rpc/reactor.cc
+++ be/src/kudu/rpc/reactor.cc
@@ -277,7 +277,7 @@ void ReactorThread::CancelOutboundCall(const shared_ptr<OutboundCall>& call) {
                      &conn)) {
     conn->CancelOutboundCall(call);
   }
-  call->Cancel();
+  call->Cancel(conn.get());
 }
 
 //
@@ -482,7 +482,7 @@ Status ReactorThread::StartConnectionNegotiation(const scoped_refptr<Connection>
   ThreadPool* negotiation_pool =
       reactor()->messenger()->negotiation_pool(conn->direction());
   RETURN_NOT_OK(negotiation_pool->SubmitClosure(
-        Bind(&Negotiation::RunNegotiation, conn, authentication, encryption, deadline)));
+      Bind(&Negotiation::RunNegotiation, conn, authentication, encryption, deadline)));
   return Status::OK();
 }
 
